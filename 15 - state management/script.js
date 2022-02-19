c8aa6bdf585b6c56708a9436653d227fc9c3a102
ResGul <reshadgulam@gmail.com>
2022-02-19 13:04:31 +0200

    15 - state management 22:37

---
+++ 15 - state management/script.js
@@ -0,0 +1,20 @@
+/** @type {HTMLCanvasElement} */
+import Player from './player.js'
+import InputHandler from './input.js'
+
+window.addEventListener('load', () => {
+  loading.style.display = 'none';
+  const canvas = document.getElementById('canvas1');
+  const ctx = canvas.getContext('2d');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  const player = new Player(canvas.width, canvas.height);
+  player.draw(ctx);
+  const input = new InputHandler();
+  
+  function animate() {
+    requestAnimationFrame(animate);
+  }
+  animate();
+})
