2852ec018d04a98f77f2ff81ba2e530703e23dde
ResGul <reshadgulam@gmail.com>
2022-02-22 15:56:21 +0200

    16 - начинаю чек player.udpate(input) 16:06

--- 16 - side scrolle/script.js
+++ 16 - side scrolle/script.js
@@ -9,23 +9,48 @@ class InputHandler {
   constructor() {
     this.keys = [];
     window.addEventListener('keydown', e => {
-      if (e.key === 'ArrowDown' && this.keys.indexOf(e.key) === -1) {// крутая тема - тут проверка на indexOf = -1 позволяет определить, есть ли дубль элемента в массиве
+      if (( e.key === 'ArrowDown' ||
+            e.key === 'ArrowUp' ||
+            e.key === 'ArrowLeft' ||
+            e.key === 'ArrowRight') &&
+            this.keys.indexOf(e.key) === -1) {// крутая тема - тут проверка на indexOf = -1 позволяет определить, есть ли дубль элемента в массиве
         this.keys.push(e.key)
       }
     })
     
     window.addEventListener('keyup', e => {
-      if (e.key === 'ArrowDown') {
+      if (e.key === 'ArrowDown' ||
+          e.key === 'ArrowUp' ||
+          e.key === 'ArrowLeft' ||
+          e.key === 'ArrowRight') {
         this.keys.splice(this.keys.indexOf(e.key), 1);
       }
+      console.log(this.keys);
     })
   }
 }
 
-const input = new InputHandler();
-
 class Player {
-
+  constructor(gameWidth, gameHeight) {
+    this.gameWidth = gameWidth;
+    this.gameHeight = gameHeight;
+    this.width = 200;
+    this.height = 200;
+    this.x = 0;
+    this.y = this.gameHeight - this.height;
+    this.image = document.getElementById('playerImage');
+    this.frameX = 0;
+    this.frameY = 0;
+    this.speed = 0;
+  }
+  draw(context) {
+    context.drawImage(this.image, this.frameX * this.width, this.frameY * this.width,this.width, this.height, this.x, this.y, this.width, this.height);
+  }
+  update(input) {
+    // horizontal movement
+    this.x += this.speed;
+    
+  }
 }
 
 class Backgrounds {
@@ -44,6 +69,13 @@ function dispalyStatusText() {
 
 }
 
-function animate() {
+const input = new InputHandler();
+const player = new Player(canvas.width, canvas.height);
 
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  player.draw(ctx);
+  player.update(input);
+  requestAnimationFrame(animate);
 }
+animate();
